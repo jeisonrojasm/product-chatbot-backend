f0cf169d98197963d7bfb1bc86390a0ca5531463
Jeison Rojas <j_esteban96@hotmail.com>
2025-07-26 10:04:28 -0500

    move product logic to its own module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { openAIProvider } from './chat.providers';
+import { ProductModule } from 'src/product/product.module';
 
 @Module({
+  imports: [ProductModule],
   controllers: [ChatController],
   providers: [
     ChatService,
---
+++ src/product/product.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import * as csv from 'csv-parser';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Product } from './interfaces/product.interface';
+
+@Injectable()
+export class ProductService {
+  private readonly logger = new Logger(ProductService.name);
+
+  private async readProductsFromCSV(filePath: string): Promise<Product[]> {
+    return new Promise((resolve, reject) => {
+      const results: Product[] = [];
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data: Product) => results.push(data))
+        .on('end', () => resolve(results))
+        .on('error', (err) => {
+          this.logger.error('Error parsing CSV file', err);
+          reject(err);
+        });
+    });
+  }
+
+  async searchProducts(query: string): Promise<Product[]> {
+    try {
+      const MAX_RESULTS = 2; // Maximum number of results to return
+      if (!query?.trim()) return [];
+
+      const filePath = path.resolve(__dirname, '..', 'assets', 'products_list.csv');
+
+      // fs.promises.access() check if the CSV file exists before trying to read it.
+      // This prevents runtime errors when attempting to read a missing file.
+      // fs.constants.F_OK is used to test for the existence of the file only.
+      // If the file does not exist, an error will be thrown and caught by the try-catch block below.
+      await fs.promises.access(filePath, fs.constants.F_OK);
+
+      const products = await this.readProductsFromCSV(filePath);
+
+      const filtered = products
+        .filter(product =>
+          `${product.displayTitle} ${product.embeddingText}`
+            .toLowerCase()
+            .includes(query.toLowerCase())
+        )
+        .slice(0, MAX_RESULTS);
+
+      return filtered;
+    } catch (error) {
+      this.logger.error('Failed to search products', error);
+      throw new InternalServerErrorException('Could not search products');
+    }
+  }
+}
